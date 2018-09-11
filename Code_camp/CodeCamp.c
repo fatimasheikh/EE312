c039c2dc9f4ca0895862efcd629325bdc3fcf7b9
Fatima Sheikh <tammysheikh@sbcglobal.net>
2018-09-11 17:26:13 -0500

    Initial commit

---
+++ Code_camp/CodeCamp.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<limits.h>
+
+// CodeCamp.c -- EE 312 Project 1
+
+/* Student information for project:
+ *
+ * Replace <NAME> with your name.
+ *
+ * On my honor, Fatima Sheikh, this programming project is my own work
+ * and I have not provided this code to any other student.
+ *
+ * Name: Fatima Sheikh
+ * email address: tammysheikh@sbcglobal.net
+ * UTEID: fs7487
+ * Section 5 digit ID: 16195
+ *
+ */
+
+
+
+/* Determine the hamming distance between two int arrays.
+   pre: aList != null, bList != null, aList.length == bList.length == len
+   post: return hamming distance between the two arrays of ints.
+   Neither parameter should be altered by this function.
+*/
+int hammingDistance(int aList[], int bList[], int len) {
+  int hamDist = 0;
+  for(int i = 0; i < len; i++) {
+    if(aList[i] != bList[i]) {   //compare each index value
+      hamDist++;
+    }
+  }
+  return hamDist;
+}
+
+/* Determine if two ints have the same last digit.
+   Note that the last digit of 0 is 0 and the last digit of -12 is 2.
+   post: return true if x and y have the same last digit, false otherwise.
+*/
+bool lastDigit(int x, int y) {
+  int xLastDigit = abs(x%10);
+  int yLastDigit = abs(y%10);
+  if (xLastDigit == yLastDigit) {   //compare last digit
+    return true;
+  }
+  return false;
+}
+
+/* Determine the sum of the positive integers less than 1000 that are multiples of 3 or 5 (or both).
+   post: return the sum of the integers from 1 to 1000 that are multiples of 3 or 5 or both.
+*/
+int sum3or5Multiples() {
+  int sum = 0;
+  for(int num = 1; num < 1000; num ++) {
+    if ((num%3) == 0) {   //check if multiple of 3
+      sum = sum + num;
+    }
+    else if ((num%5) == 0) { //if not multiple of 3, check 5
+      sum = sum + num;
+    }
+  }
+  return sum;
+}
+
+/* Reverse the digits of an integer. Return the reversed number if it is in the range of type int, 0 otherwise.
+   post: return x with digits reversed if the reverse can be stored as an int, 0 otherwise.
+*/
+int reverseInt(int x) {
+  long long int reverse = 0;
+  while (x != 0) {
+    int digit = x%10;
+    reverse = (reverse*10)+digit;   //shift last digit to left and add new digit
+    x = (x-digit)/10;
+    if (llabs(reverse) > INT_MAX) {  //test for integer overflow
+      return 0;
+    }
+  }
+  return reverse;
+}
---
+++ Code_camp/CodeCampTester.c
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<stdbool.h>
+int hammingDistance(int aList[], int bList[], int len);
+int sum3or5Multiples();
+bool lastDigit(int, int);
+int reverseInt(int);
+
+// CodeCampTester.c -- EE 312 Project 1 -- Tester class
+
+/* Student information for assignment:
+ *
+ * Name: Fatima Sheikh
+ * email address: tammysheikh@sbcglobal.net
+ * UTEID: fs7487
+ * Section 5 digit ID: 16195
+ *
+ */
+
+int main() {
+  // my test 1, hammingDistance
+  int h1[] = {1, 3, 3, 4, -8, -7};
+  int h2[] = {1, 2, 10, 4, 5, 4};
+  int expected = 4;
+  int actual = hammingDistance(h1, h2, 6);
+  printf("My Test 1 hamming distance: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 1, hamming distance\n\n");
+  else printf("**** FAILED **** my test 1, hamming distance\n\n");
+
+  // my test 2, hamming distance
+  int h3[] = {0, 6, 3, 3, 3, 15, -4, 13, -5, 4};
+  int h4[] = {-6, 6, 3, -6, 9, 2, 21, 2, -6, -6};
+  expected = 8;
+  actual = hammingDistance(h3, h4, 10);
+  printf("My Test 2 hamming distance: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 2, hamming distance\n\n");
+  else printf("**** FAILED **** my test 2, hamming distance\n\n");
+
+  // my test 3, hamming distance
+  int h5[] = {1,1,1};
+  int h6[] = {-1,-1,-1};
+  expected = 3;
+  actual = hammingDistance(h5, h6, 3);
+  printf("My Test 3 hamming distance: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 3, hamming distance\n\n");
+  else printf("**** FAILED **** my test 3, hamming distance\n\n");
+
+  // my test 4, last digit
+  int n1 = 5;
+  int n2 = 1325;
+  expected = true;
+  actual = lastDigit(n1, n2);
+  printf("My Test 4 last digit: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 4, last digit\n\n");
+  else printf("**** FAILED **** my test 4, last digit\n\n");
+
+  // my test 5, last digit
+  n1 = -1;
+  n2 = 15;
+  expected = false;
+  actual = lastDigit(n1, n2);
+  printf("My Test 5 last digit: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 5, last digit\n\n");
+  else printf("**** FAILED **** my test 5, last digit\n\n");
+
+  // my test 6, last digit
+  n1 = 148;
+  n2 = 38;
+  expected = true;
+  actual = lastDigit(n1, n2);
+  printf("My Test 6 last digit: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 6, last digit\n\n");
+  else printf("**** FAILED **** my test 6, last digit\n\n");
+
+  // test 7, sum of multiples of 3 or 5
+  expected = 233168;
+  actual = sum3or5Multiples();
+  printf("Test 7 sum of multiples: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed test 7, sum of multiples of 3 or 5\n\n");
+  else printf("**** FAILED **** test 7, sum of multiples of 3 or 5\n\n");
+
+  // my test 8, reverse int
+  n1 = -639;
+  expected = -936;
+  actual = reverseInt(n1);
+  printf("My Test 8 reverse int: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 8, reverse int\n\n");
+  else printf("**** FAILED **** my test 8, reverse int\n\n");
+
+  // My test 9, reverse int
+  n1 = 500;
+  expected = 5;
+  actual = reverseInt(n1);
+  printf("My Test 9 reverse int: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 9, reverse int\n\n");
+  else printf("**** FAILED **** My test 9, reverse int\n\n");
+
+  // My test 10, reverse int
+  n1 = -50934;
+  expected = -43905;
+  actual = reverseInt(n1);
+  printf("My Test 10 reverse int: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 10, reverse int\n\n");
+  else printf("**** FAILED **** my test 10, reverse int\n\n");
+
+  // My test 11, reverse int
+  n1 = -1147483647;
+  expected = 0;
+  actual = reverseInt(n1);
+  printf("My Test 11 reverse int: expected value: %d, actual value: %d\n", expected, actual);
+
+  if(expected == actual)  printf("passed my test 11, reverse int\n");
+  else printf("**** FAILED **** my test 11, reverse int\n");
+
+}
